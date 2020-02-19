196f1713f5fba203bddfed634d4564ef4e202493
josephkem <josephkemoy@gmail.com>
2020-02-19 10:53:22 -0500

    Modified the post view so that each post renders with a link which takes the user to the page with that post using the id

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
             @foreach ($posts as $post)
             
                 <li class="list-group-item">
-                <h3>{{$post->title}}</h3>
+                <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
                 <small>Written on {{$post->created_at}}</small>
                 </li>
                 
