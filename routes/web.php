d77ed579a2a3177771475b93c390fca2451462ce
josephkem <josephkemoy@gmail.com>
2020-02-19 09:34:06 -0500

    Fixed an error with mapping the routes to the posts

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 
-Route::resource('posts', 'PostController');
+Route::resource('posts', 'PostsController');
 
 
