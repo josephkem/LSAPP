52345ce74a3b578fa7b5281316262fa423739da3
josephkem <josephkemoy@gmail.com>
2020-02-19 10:20:55 -0500

    Modified the post view to change the layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,5 +3,15 @@
 
 @section('content')
     <h1>Posts</h1>
+    @if (count($posts) > 0)
+        @foreach ($posts as $post)
+
+            <div class="well">
+            <h3>{{$post->title}}</h3>
+            </div>
+            
+        @endforeach
+        
+    @endif
 
 @endsection
