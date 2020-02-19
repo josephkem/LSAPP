d68961d0da6e59f8aa8bbec4629ce0f7228949db
josephkem <josephkemoy@gmail.com>
2020-02-19 11:20:58 -0500

    Added the bodt of the post to the show view of the posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,12 @@
 
 
 @section('content')
-<h1>{{$post->title}}</h1>
+    <h1>{{$post->title}}</h1>
+    <small>Written on {{$post->created_at}}</small>
+
+    <div>
+        {{$post->body}}
+    </div>
+    
    
 @endsection
