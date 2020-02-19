2591a4c306bc20f5d2088f98e58f62a5a4386e7f
josephkem <josephkemoy@gmail.com>
2020-02-19 10:35:05 -0500

    Modified the navbar file to display the link to the post route

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -19,6 +19,9 @@
           <li class="nav-item">
             <a class="nav-link" href="/services">Services</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts">Blog</a>
+          </li>
         
         
       </ul>
