9adc440bb6bfe7d2e42d49940bcc6ee0b3f5fad7
josephkem <josephkemoy@gmail.com>
2020-02-20 03:03:35 -0500

    Used the laravel collective syntax to create part of the form for the posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,5 +3,12 @@
 
 @section('content')
     <h1>Create Post</h1>
+    {!! Form::open(['action'=>'PostsController@store', 'method' => 'POST']) !!}
+        <div class="">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+
+        </div>
+    {!! Form::close() !!}
    
 @endsection
