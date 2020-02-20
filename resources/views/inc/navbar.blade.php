d98c90427243724b6a0808a5c2c5a1af4a390251
josephkem <josephkemoy@gmail.com>
2020-02-20 03:50:38 -0500

    Added a link in the navbar to direct the user to the creat post page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,9 +1,6 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <a class="navbar-brand" href="#">{{config('app.name', 'LSAPP')}}</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
   
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
@@ -24,6 +21,13 @@
           </li>
         
         
+      </ul>
+
+      <ul class="navbar navbar-nav navbar-right">
+          <li>
+              <a href="/posts/create">Create Post</a>
+          </li>
+
       </ul>
       
     </div>
