def42440644a635c08ce5fa8b2dd8d9dc2815a90
josephkem <josephkemoy@gmail.com>
2020-02-21 00:35:52 -0500

    Fixed an error that prevented the edit post page from rendering

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,18 +1,21 @@
+@extends('layouts.app')
+
+
 @section('content')
     <h1>Edit Post</h1>
     {!! Form::open(['action'=>['PostsController@update', $post->id], 'method' => 'POST']) !!}
         <div class="">
             {{Form::label('title', 'Title')}}
-            {{Form::text('title', '$post->title', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+            {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
 
         </div>
         </br>
         <div class="">
             {{Form::label('body', 'Body')}}
-            {{Form::textarea('body', '$post->body', ['id'=>'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
+            {{Form::textarea('body', $post->body, ['id'=>'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
 
         </div></br>
-        {{Form::hidden('_method', 'PUT')}}
+        {{Form::hidden('_method','PUT')}}
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
 
