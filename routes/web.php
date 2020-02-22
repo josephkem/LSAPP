6cabd2092855df48311b8e5b1713cdd797aaff98
josephkem <josephkemoy@gmail.com>
2020-02-22 13:32:03 -0500

    Used artisan to enable the user authentication in the project

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/services', 'PagesController@services');
 Route::resource('posts', 'PostsController');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
